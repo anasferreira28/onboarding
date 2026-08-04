8039ff1c303cee664d5286f17c1bfdc2a6a0c384
anasferreira28 <ana_sf@outlook.pt>
2026-08-04 10:58:39 +0100

    a few adjustements to dataset loading

--- pubmedqa_setup.py
+++ pubmedqa_setup.py
@@ -2,17 +2,17 @@
 Import the PubMedQA dataset from Hugging Face for Agent building.
 Includes an example to print the first question and its final decision.
 """
-# from dotenv import load_dotenv
-# load_dotenv() # reads .env from the current folder into the environment - if we want to load the HF token
+from dotenv import load_dotenv
+load_dotenv() # reads .env from the current folder into the environment - if we want to load the HF token
 
 from datasets import load_dataset
 
 pubmedqa = load_dataset("qiaojin/PubMedQA", "pqa_labeled")["train"]
 
-# print(pubmedqa[0]["question"])
-# print(pubmedqa[0]["final_decision"])
-# # Q: Do mitochondria play a role in remodelling lace plant leaves during programmed cell death?
-# # A: yes
+print(pubmedqa[0]["question"])
+print(pubmedqa[0]["final_decision"])
+# Q: Do mitochondria play a role in remodelling lace plant leaves during programmed cell death?
+# A: yes
 
 ## NOTE:
 # A token is typically only needed for:
