b7fb84de863914e689d989751aa27c483d5523c4
anasferreira28 <ana_sf@outlook.pt>
2026-08-10 10:43:52 +0100

    Fixes to exercise 5.3

--- exercise5/agent.py
+++ exercise5/agent.py
@@ -11,13 +11,13 @@ from local_models import gemma_model
 # Dataset Startup
 from datasets import load_dataset
 ds = load_dataset("qiaojin/PubMedQA", "pqa_labeled")["train"]
-corpus = {example["pubid"]: " ".join(example["context"]["contexts"]) for example in ds}
+corpus = {str(example["pubid"]): " ".join(example["context"]["contexts"]) for example in ds} # corpus is originally int, so must be converted to string to it matches the requirements of the prompt and function call
 
 class PubMedAnswer(BaseModel):
     question: str
     decision: Literal["yes", "no", "maybe"]
     rationale: str
-    confidence: int # 0-100
+    confidence: Literal["low", "medium", "high"]
 
 @function_tool
 def fetch_abstract(pubid: str) -> str:
@@ -32,7 +32,7 @@ qa_agent = Agent(
     instructions = (
         "You answer biomedical research questions using ONLY the abstract of a PubMed article provided to you. "
         "Do not use outside knowledge."
-        "Give a YES/NO/MAYBE decision, a one-sentence rationale grounded in the context, and a 0-100 confidence score for your answer."
+        "Give a YES/NO/MAYBE decision, a one-sentence rationale grounded in the context, and a confidence level (low, medium, or high) for your answer."
     ),
     model = gemma_model,
     output_type = PubMedAnswer,
@@ -70,3 +70,12 @@ example = ds[0]
 # Example 2
 result = Runner.run_sync(qa_agent_tool, f"pubid: {example['pubid']}\nQuestions: {example['question']}")
 print(result.final_output)
+
+# TRACING CONFIRMS FUNCTION CALL
+# [SPAN START] FunctionSpanData: <agents.tracing.span_data.FunctionSpanData object at 0x00000203C9412A80>
+# [SPAN END]   FunctionSpanData: <agents.tracing.span_data.FunctionSpanData object at 0x00000203C9412A80>
+
+# question='Do mitochondria play a role in remodelling lace plant leaves during programmed cell death?' 
+# decision='maybe' 
+# rationale='The provided PubMed abstract for pubid: 21645374 was not found, so I cannot determine the answer based on the retrieved evidence.' 
+# confidence=0
