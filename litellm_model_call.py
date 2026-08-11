a6efb98542dcd34cf18e358de5ae29ee606d0f39
anasferreira28 <ana_sf@outlook.pt>
2026-08-11 10:07:34 +0100

    fixed api base redicting link

--- litellm_model_call.py
+++ litellm_model_call.py
@@ -7,14 +7,14 @@ import litellm
 response_gemma = litellm.completion(
     model = "ollama_chat/gemma4:e2b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
     messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
-    api_base="https://localhost:11434" # Ollama's fixed default address
+    api_base="http://localhost:11434" # Ollama's fixed default address
 )
 print(response_gemma.choices[0].message.content)
 
 response_qwen = litellm.completion(
     model = "ollama_chat/qwen3:8b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
     messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
-    api_base="https://localhost:11434" 
+    api_base="http://localhost:11434"
 )
 print(response_qwen.choices[0].message.content)
 
