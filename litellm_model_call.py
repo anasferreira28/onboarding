5c250fa81aa83449100a3988023c25646a92f4e2
anasferreira28 <ana_sf@outlook.pt>
2026-08-11 10:31:36 +0100

    final version of the litellm exercise

--- litellm_model_call.py
+++ litellm_model_call.py
@@ -4,12 +4,12 @@ Exercise 2.5: LiteLLM call for 2 local Gemma models
 
 import litellm
 
-response_gemma = litellm.completion(
-    model = "ollama_chat/gemma4:e2b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
-    messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
-    api_base="http://localhost:11434" # Ollama's fixed default address
-)
-print(response_gemma.choices[0].message.content)
+# response_gemma = litellm.completion(
+#     model = "ollama_chat/gemma4:e2b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
+#     messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
+#     api_base="http://localhost:11434" # Ollama's fixed default address
+# )
+# print(response_gemma.choices[0].message.content)
 
 response_qwen = litellm.completion(
     model = "ollama_chat/qwen3:8b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
