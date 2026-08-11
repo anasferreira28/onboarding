ded779e69ef6da9dbb0168bdb83d904e2ac5afff
anasferreira28 <ana_sf@outlook.pt>
2026-08-11 10:03:16 +0100

    changed script name to avoid collision with the package name

--- litellm_model_call.py
+++ litellm_model_call.py
@@ -2,16 +2,16 @@
 Exercise 2.5: LiteLLM call for 2 local Gemma models
 """
 
-import litellm
+import litellm_model_call
 
-response_gemma = litellm.completion(
+response_gemma = litellm_model_call.completion(
     model = "ollama_chat/gemma4:e2b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
     messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
     api_base="https://localhost:11434" # Ollama's fixed default address
 )
 print(response_gemma.choices[0].message.content)
 
-response_qwen = litellm.completion(
+response_qwen = litellm_model_call.completion(
     model = "ollama_chat/qwen3:8b", # the 'ollama_chat' applies Ollama's chat template correctly for instruction models
     messages = [{"role": "user", "content": "Explain BI-RADSA categories briefly."}],
     api_base="https://localhost:11434" 
